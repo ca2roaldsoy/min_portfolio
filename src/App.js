c840f1a5661aa738d4e43c197bc199e9b4382fa6
ca2roaldsoy <roaldsoyen@gmail.com>
2020-11-04 22:13:28 +0100

    added background animation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import React from "react";
+import ParticlesBg from "particles-bg";
+import Home from "./components/home/Home";
+
 function App() {
   return (
     <div className="App">
- 
+         <ParticlesBg type="cobweb" bg={true} color="#c9c9c9" />
+         <Home />
     </div>
   );
 }
